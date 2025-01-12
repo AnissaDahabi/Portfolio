65fb89853711a37c564d946140c8ee2b065a268c
Anissa Dahabi <anissa.Dahabi@gmail.com>
2025-01-12 16:56:16 +0100

    All windows all created and functional

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+/* A propos */
+
 function openWindowAPropos() {
     let a = document.getElementById("aProposWindow");
     if (a.style.display === "none") {
@@ -17,3 +19,82 @@ function closeWindowAPropos() {
     }
 }
 
+/* Expériences professionnelles */
+
+function openWindowExpPro() {
+    let a = document.getElementById("expProWindow");
+    if (a.style.display === "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+}
+
+function closeWindowExpPro() {
+    let c = document.getElementById("expProWindow");
+    if (c.style.display === "block") {
+        c.style.display = "none";
+    } else {
+        c.style.display = "block";
+    }
+}
+
+/* Mes Projets */
+
+function openWindowProjets() {
+    let a = document.getElementById("projetsWindow");
+    if (a.style.display === "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+}
+
+function closeWindowProjets() {
+    let c = document.getElementById("projetsWindow");
+    if (c.style.display === "block") {
+        c.style.display = "none";
+    } else {
+        c.style.display = "block";
+    }
+}
+
+/* Liens */
+
+function openWindowLiens() {
+    let a = document.getElementById("liensWindow");
+    if (a.style.display === "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+}
+
+function closeWindowLiens() {
+    let c = document.getElementById("liensWindow");
+    if (c.style.display === "block") {
+        c.style.display = "none";
+    } else {
+        c.style.display = "block";
+    }
+}
+
+/* Contact */
+
+function openWindowContact() {
+    let a = document.getElementById("contactWindow");
+    if (a.style.display === "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+}
+
+function closeWindowContact() {
+    let c = document.getElementById("contactWindow");
+    if (c.style.display === "block") {
+        c.style.display = "none";
+    } else {
+        c.style.display = "block";
+    }
+}
